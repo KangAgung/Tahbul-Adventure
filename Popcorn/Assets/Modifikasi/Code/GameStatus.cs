5035ca2d9588fb3aae6b2ff82aacf22cd8a8ea5f
Unknown <sawmaster22@gmail.com>
2019-09-30 09:02:13 +0700

    Penambahan life

---
+++ Popcorn/Assets/Modifikasi/Code/GameStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    public static int score = 0;
+    //public static int currentLevel = 1;
+    public static int lives = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
